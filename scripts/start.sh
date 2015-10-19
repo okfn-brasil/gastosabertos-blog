a428b6d524c2cfb763917cce7e1088cae7519756
RenatoCRON <renato.cron@gmail.com>
2015-10-19 14:25:20 -0200

    always rewrite php.ini

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+mv /etc/php5/apache2/php.ini /etc/php5/apache2/php.ini.orig
+sed "s/upload_max_filesize = 2M/upload_max_filesize = 32M/" /etc/php5/apache2/php.ini.orig > /etc/php5/apache2/php.ini
+
 if [ ! -f /var/www/wp-config.php ]; then
 
 # sed "s/datadir  = \/var\/lib\/mysql/datadir = /data\/mysql/" /etc/mysql/my.cnf > /etc/mysql/my.cnf
@@ -9,13 +13,13 @@ cp -dpr /wordpress/* /var/www
 chown -R www-data:www-data /var/www/
 #mysql has to be started this way as it doesn't work to call from /etc/init.d
 
-/usr/bin/mysqld_safe & 
+/usr/bin/mysqld_safe &
 sleep 10s
 # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
 WORDPRESS_DB="wordpress"
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
 WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-#This is so the passwords show up in logs. 
+#This is so the passwords show up in logs.
 echo mysql root password: $MYSQL_PASSWORD
 echo wordpress password: $WORDPRESS_PASSWORD
 echo $MYSQL_PASSWORD > /mysql-root-pw.txt
@@ -37,13 +41,9 @@ s/password_here/$WORDPRESS_PASSWORD/
 /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
 /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /var/www/wp-config-sample.php > /var/www/wp-config.php
 
-mv /etc/php5/apache2/php.ini /etc/php5/apache2/php.ini.orig
-sed "s/upload_max_filesize = 2M/upload_max_filesize = 20M/" /etc/php5/apache2/php.ini.orig > /etc/php5/apache2/php.ini
-
- 
 
 chown www-data:www-data /var/www/wp-config.php
-mysqladmin -u root password $MYSQL_PASSWORD 
+mysqladmin -u root password $MYSQL_PASSWORD
 mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
 killall mysqld
 sleep 10s
